7ca01f7149e64aa7914af43fe4f934aa4c583e37
Dev <devsharma23101999@gail.com>
2020-06-23 16:01:54 +0530

    notification

--- app/src/main/java/com/example/mychatapp/MainActivity.java
+++ app/src/main/java/com/example/mychatapp/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.mychatapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +28,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -32,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseListAdapter<ChatMessage> adapter;
     private static String TAG="MainActivity";
     RecyclerView listOfMessages;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,6 +107,25 @@ public class MainActivity extends AppCompatActivity {
                 input.setText("");
             }
         });
+
+        TextInputEditText input = (TextInputEditText)findViewById(R.id.input);
+        listOfMessages= (RecyclerView)findViewById(R.id.list_of_messages);
+        listOfMessages.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v,
+                                       int left, int top, int right, int bottom,
+                                       int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (bottom < oldBottom) {
+                    listOfMessages.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            listOfMessages.smoothScrollToPosition(
+                                    Math.max((listOfMessages.getAdapter().getItemCount() - 1),0));
+                        }
+                    }, 100);
+                }
+            }
+        });
     }
 
     private void showMessage(String message) {
@@ -102,10 +133,11 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,message, Toast.LENGTH_LONG).show();
 
     }
-
+    int f=0;
     public void displayChatMessages(){
         listOfMessages = (RecyclerView)findViewById(R.id.list_of_messages);
-        listOfMessages.setLayoutManager(new LinearLayoutManager(this));
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        listOfMessages.setLayoutManager(linearLayoutManager);
 
         FirebaseDatabase.getInstance().getReference().addValueEventListener(new ValueEventListener() {
             @Override
@@ -119,13 +151,21 @@ public class MainActivity extends AppCompatActivity {
 
                 MessageAdapter messageAdapter = new MessageAdapter(getApplicationContext(),MessageList);
                 listOfMessages.setAdapter(messageAdapter);
+                listOfMessages.scrollToPosition(MessageList.size()-1);
+                if(f==1){
+                    ChatMessage newMessage = MessageList.get(MessageList.size()-1);
+                    if(newMessage.getMessageUser() != FirebaseAuth.getInstance().getCurrentUser().getDisplayName()){
+                        sendNotification(newMessage.getMessageText(), newMessage.getMessageUser());
+                    }
+                }
+                f=1;
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
+
     }
 
     @Override
@@ -151,5 +191,36 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void sendNotification(String messageBody, String messageTitle) {
+        Log.d(TAG,"Entered notification sender");
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        String channelId = "101";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                        .setContentTitle(messageTitle)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent)
+                        .setPriority(Notification.PRIORITY_MAX);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(1 /* ID of notification */, notificationBuilder.build());
     }
 }
